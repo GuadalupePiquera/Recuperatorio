ca1161061cd98f9b4f9579da6083fae67273a82b
GuadalupePiquera <guadapiquera4@gmail.com>
2022-10-20 15:28:26 -0300

    modifico app.js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const express = require('express');
-
 const app = express();
+const port = process.env.PORT  || 2020;
+
+app.use(express.static('public'));
+
+
+app.listen (process.env.PORT ||2020, ()=>{
+    console.log('Abriendo el servidor '+port);
+});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
