24498d45196a178540628188dec26ea1606240a7
GuadalupePiquera <guadapiquera4@gmail.com>
2022-10-24 18:06:14 -0300

    arreglo detalles

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const path = require('path');
 const express = require('express');
-const server = express();
+const app = express();
+const port = process.env.PORT  || 2020;
 
+app.listen(port, () => console.log('Abriendo el servidor'+port));
 
-const port = process.env.PORT || 2020;
-server.listen(port, () => console.log('Abriendo el servidor http://localhost:'+port));
+app.use(express.static('public'));
 
-const public = path.resolve(__dirname, './public');
-
-server.use(express.static(public));
-
-server.get('/', (req,res) => res.sendFile(path.resolve(__dirname, './views/home.html')))
-server.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, './views/login.html')))
-server.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, './views/register.html')))
-
-server.get('/', (req,res) => res.send('Bienvenid@ a Mercado Liebre'));
+app.get('/', (req,res) => res.sendFile(path.resolve(__dirname, 'views/home.html')))
+app.get('/register', (req,res) => res.sendFile(path.resolve(__dirname, 'views/register.html')))
+app.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, 'views/login.html')))
